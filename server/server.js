ba34bd1d266d61e509aec1b5e300de5a9c34740d
Dat Tan <dattan2911@gmail.com>
2022-12-02 21:52:03 +0700

    fix CORS policy

--- server/server.js
+++ server/server.js
@@ -17,6 +17,12 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
 const app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
