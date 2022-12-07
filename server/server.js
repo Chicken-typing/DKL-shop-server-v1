ee3a8a054cdaa354de93b72327f9dd6ca49e2c2a
Dat Tan <dattan2911@gmail.com>
2022-12-07 20:58:46 +0700

    fix CORS policy

--- server/server.js
+++ server/server.js
@@ -50,25 +50,29 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*"}});
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  }
+});
 
-  io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-  
-    socket.on("join_room", (data) => {
-      socket.join(data);
-      console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
-  
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
-    });
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
-  
-  server.listen(3000, () => {
-    console.log("SERVER RUNNING");
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
   });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("SERVER RUNNING");
+});
