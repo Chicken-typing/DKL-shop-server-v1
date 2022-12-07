c697eb3499d8d0f67268cda45c3623f07bf3e63d
Dat Tan <dattan2911@gmail.com>
2022-12-07 22:12:39 +0700

    remove chat

--- server/server.js
+++ server/server.js
@@ -43,36 +43,6 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5000;
-// app.listen(port, () => {
-//   console.log(`server listening on port: ${port}`);
-// })
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "https://staging-dkl.netlify.app",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-  }
-});
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
-
-server.listen(3000, () => {
-  console.log("SERVER RUNNING");
-});
+app.listen(port, () => {
+  console.log(`server listening on port: ${port}`);
+})
