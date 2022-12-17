0b351b89a39ef23e333a1ffd9d267b92bbef8e25
Kreal244 <nuunkhang@gmail.com>
2022-12-18 02:46:08 +0700

    add email

--- server/Router/productRoute.js
+++ server/Router/productRoute.js
@@ -86,14 +86,15 @@ productRoute.post(
     const productId = req.params.id;
     const product = await Product.findById(productId);
     if (product) {
-      if (product.reviews.find((x) => x.name === req.user.username)) {
+      if (product.reviews.find((x) => x.email === req.user.email)) {
         return res
           .status(400)
-          .send({ message: 'You already submitted a review' });
+          .send({ message: "You already submitted a review" });
       }
 
       const review = {
         name: req.user.username,
+        email: req.user.email,
         rating: Number(req.body.rating),
         comment: req.body.comment,
       };
