5e2de22f5d5d75776ef3b5919742bde47e1b7b97
Dat Tan <dattan2911@gmail.com>
2022-12-18 00:48:21 +0700

    add review and rating

--- server/server.js
+++ server/server.js
@@ -46,33 +46,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server listening on port: ${port}`);
 })
-
-// const server = http.createServer(app);
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   }
-// });
-
-// io.on("connection", (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
-
-//   socket.on("join_room", (data) => {
-//     socket.join(data);
-//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-//   });
-
-//   socket.on("send_message", (data) => {
-//     socket.to(data.room).emit("receive_message", data);
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("User Disconnected", socket.id);
-//   });
-// });
-
-// server.listen(3001, () => {
-//   console.log("SERVER RUNNING");
-// });
