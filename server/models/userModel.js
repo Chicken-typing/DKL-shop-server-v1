a0c1e07cfda238dd13335272c263c8c3d441bf22
dattan2911 <49641931+dattan2911@users.noreply.github.com>
2022-12-18 17:05:29 +0700

    update userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
     {
-        username: {type: String, unique: true,},
+        username: {type: String, required: true,},
         email: {type: String, unique: true, required: true,},
         password: { type: String, required: true,},
         role: { type: String, required: true,},
